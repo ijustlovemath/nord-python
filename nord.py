c6521ab63693ed07c3d2cc4e53e3a8f08568105c
Jeremy DeJournett <dejourn2@illinois.edu>
2019-08-20 17:18:44 -0400

    reorganizing, adding support for openvpn2.4 style directories

--- nord.py
+++ nord.py
@@ -3,12 +3,6 @@ import argparse
 import urllib.request
 import json
 
-url = "https://nordvpn.com/wp-admin/admin-ajax.php?action=servers_recommendations"
-request = urllib.request.Request(url=url, headers={"User-Agent":"Mozilla/5.0"})
-raw_json = urllib.request.urlopen(request).read()
-recommendations = json.loads(raw_json)
-server = recommendations[0]["hostname"]
-
 protocols = ["tcp", "udp"]
 
 port_lookup = {
@@ -18,55 +12,84 @@ port_lookup = {
 
 base_config_path = "/etc/openvpn/ovpn_"
 
-print(f"echo Recommended server identified: {server}")
+options = lambda:0
+options.openvpn24 = False
 
-for protocol in protocols:
-    port = port_lookup[protocol]
-    config_file = f"{server}.{protocol}{port}.ovpn"
-    config_file_url = ("https://downloads.nordcdn.com/configs/files/" 
-            + f"ovpn_legacy/servers/{config_file}"
+def recommended_server():
+    url = "https://nordvpn.com/wp-admin/admin-ajax.php?action=servers_recommendations"
+    request = urllib.request.Request(url=url, headers={"User-Agent":"Mozilla/5.0"})
+    raw_json = urllib.request.urlopen(request).read()
+    recommendations = json.loads(raw_json)
+    return recommendations[0]["hostname"]
+
+def install_dir(protocol):
+    if options.openvpn24:
+        return "/etc/openvpn/client"
+    return f"{base_config_path}{protocol}"
+
+def main(args):
+    global options
+
+    parser = argparse.ArgumentParser(
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
 
-    print(f"echo Downloading VPN configuration for {protocol.upper()}")
-    print(f"wget {config_file_url} >/dev/null 2>&1")
-    print(f"echo \"Installing VPN configuration for {protocol.upper()} (requires sudo)\"")
-    print(f"sudo mv {config_file} {base_config_path}{protocol}/"
-        + f"{server}.{protocol}.ovpn"
+    parser.add_argument("--openvpn24"
+        , action="store_true"
+        , help="install VPN configurations to /etc/openvpn/client instead of /etc/openvpn/ovpn_{tcp,udp}"
     )
 
-parser = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter
-)
+    group = parser.add_mutually_exclusive_group()
 
-group = parser.add_mutually_exclusive_group()
+    group.add_argument("--tcp"
+            , action="store_true"
+            , help="automatically connect to the server using TCP"
+    )
 
-group.add_argument("--tcp"
-        , action="store_true"
-        , help="automatically connect to the server using TCP"
-)
+    group.add_argument("--udp"
+            , action="store_true"
+            , help="automatically connect to the server using UDP"
+    )
 
-group.add_argument("--udp"
-        , action="store_true"
-        , help="automatically connect to the server using UDP"
-)
+    options = parser.parse_args(args)
 
-options = parser.parse_args(sys.argv[1:])
+    server = recommended_server()
 
-for protocol in protocols:
-    locals()[f"{protocol}_connect_cmd"] = f"sudo openvpn {base_config_path}{protocol}/{server}.{protocol}.ovpn"
+    print(f"echo Recommended server identified: {server}")
 
-if options.tcp or options.udp:
-    print("echo Connecting automatically...")
-    if options.tcp:
-        print(tcp_connect_cmd)
-
-    if options.udp:
-        print(udp_connect_cmd)
-
-else:
+    # Download and install
     for protocol in protocols:
-        print("echo To connect with {upper}: {command}".format(
-                upper=protocol.upper()
-                , command=locals()[protocol + "_connect_cmd"]
-            )
+        port = port_lookup[protocol]
+        config_file = f"{server}.{protocol}{port}.ovpn"
+        config_file_url = ("https://downloads.nordcdn.com/configs/files/" 
+                + f"ovpn_legacy/servers/{config_file}"
         )
+
+        print(f"echo Downloading VPN configuration for {protocol.upper()}")
+        print(f"wget -nv {config_file_url}")
+        print(f"echo \"Installing VPN configuration for {protocol.upper()} (requires sudo)\"")
+        print(f"sudo mv {config_file} {install_dir(protocol)}/"
+            + f"{server}.{protocol}.ovpn"
+        )
+
+    for protocol in protocols:
+        locals()[f"{protocol}_connect_cmd"] = f"sudo openvpn {install_dir(protocol)}/{server}.{protocol}.ovpn"
+
+    if options.tcp or options.udp:
+        print("echo Connecting automatically...")
+        if options.tcp:
+            print(tcp_connect_cmd)
+
+        if options.udp:
+            print(udp_connect_cmd)
+
+    else:
+        for protocol in protocols:
+            print("echo To connect with {upper}: {command}".format(
+                    upper=protocol.upper()
+                    , command=locals()[protocol + "_connect_cmd"]
+                )
+            )
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
