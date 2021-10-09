3ca2cc5d15bdf3e5e1fd53dc71756585828b0c10
Jeremy DeJournett <jcdejournett@gmail.com>
2021-10-08 20:37:58 -0400

    Added automated connection binary
    Need to prompt for username and password on install if they dont yet exist

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 sudo cp vpn-killswitch.service /etc/systemd/system/
 sudo cp kill-if-disconnected.sh /usr/local/bin/
 sudo systemctl enable vpn-killswitch.service --now
+sudo cp nord-connect /usr/local/bin
